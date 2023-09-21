d501d74a2f2f47a5800770a0d6203942278659e7
ismail709 <ismailrhilal@gmail.com>
2023-09-21 04:17:54 +0100

    first commit

---
+++ ex01.py
@@ -0,0 +1,35 @@
+# this function takes a list and an integer and return the indices of two numbers in the list that add up to the target number
+# nums : list containing the numbers
+# target : integer
+def targetSum(nums, target):
+    # we copy the content of the list so we don't override the indices
+    numsCopy = nums[:]
+    # we sort the copy
+    numsCopy.sort()
+    # we delete values greater than the target
+    numsCopy = [x for x in numsCopy if x < target]
+    # we create all possible pairs to test them
+    # it may be more efficent to start testing the possible pairs before finding them all
+    numsPairs = [[numsCopy[x],numsCopy[y]] for x in range(len(numsCopy)) for y in range(len(numsCopy)) if y > x]
+    # we search for the wanted pair
+    for p in numsPairs:
+        if sum(p) == target:
+            # we return the indices of the two numbers that validate the condition
+            return [nums.index(p[0]),nums.index(p[1])]
+    else:
+        # if none the pairs pass, we return an empty list
+        return []
+
+
+
+# an example list
+myList = [1,2,4,5,9,7]
+# an example target
+myTarget = 8
+# expected result [0,5]
+result = targetSum(myList,myTarget)
+
+# we print the result
+print("the result is",result)
+
+# OUTPUT: the result is [0, 5]
